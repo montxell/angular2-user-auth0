1988b327adf242e729cd0ade145caf3b5c85aa1a
Montse Ariso <montxelltech@gmail.com>
2017-06-12 19:23:11 +0200

    Add 'login' method and button to 'navbar'

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor( private auth: AuthService ) { }
 
   ngOnInit() {
+  }
+
+
+  login() {
+    this.auth.login();
   }
 
 }
