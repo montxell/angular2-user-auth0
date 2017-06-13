cc9f2fd644e081ba1cffcc5ae5a416da8e33b01b
Montse Ariso <montxelltech@gmail.com>
2017-06-14 00:29:36 +0200

    Add code to request user's profile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,13 +6,15 @@ import auth0 from 'auth0-js';
 @Injectable()
 export class AuthService {
 
+  public userProfile: any;
+
   auth0 = new auth0.WebAuth({
     clientID: 'h4g6vCh7UHeN0e0GAtdKXp39RZU3ztW0',
     domain: 'montxell.eu.auth0.com',
     responseType: 'token id_token',
     audience: 'https://montxell.eu.auth0.com/userinfo',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
 
   constructor(public router: Router) {}
@@ -57,6 +59,21 @@ export class AuthService {
     // access token's expiry time
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+
+  public getProfile(cb): void {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('Access token must exist to fetch profile');
+    }
+
+    const self = this;
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
   }
 
 }
